51400df4d34335aa61667e1570cb584e16941119
Sofia Ramirez Leiva <sofiaramirezleiva157@gmail.com>
2024-07-22 11:40:55 +0200

    Create method Appoinments

--- Patient/src/main/java/Veterinary/controller/AppointmentsController.java
+++ Patient/src/main/java/Veterinary/controller/AppointmentsController.java
@@ -1,5 +1,6 @@
 package Veterinary.controller;
 
+import Veterinary.model.Appointments;
 import Veterinary.service.AppointmentsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
@@ -11,5 +12,10 @@ import org.springframework.web.bind.annotation.*;
 public class AppointmentsController {
     @Autowired
     AppointmentsService appointmentsService;
+
+    @PostMapping(path = "/appointments")
+    public Appointments createAppointments(@RequestBody Appointments appointment) {
+        return appointmentsService.createAppointment(appointment);
+    }
 
 }
--- Patient/src/main/java/Veterinary/service/AppointmentsService.java
+++ Patient/src/main/java/Veterinary/service/AppointmentsService.java
@@ -1,6 +1,7 @@
 package Veterinary.service;
 
 import Veterinary.Repository.AppointmentsRepository;
+import Veterinary.model.Appointments;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -9,5 +10,10 @@ import org.springframework.stereotype.Service;
 public class AppointmentsService {
     @Autowired
     AppointmentsRepository appointmentsRepository;
+
+    public Appointments createAppointment(Appointments appointment) {
+        return appointmentsRepository.save(appointment);
+    }
+
 
 }
