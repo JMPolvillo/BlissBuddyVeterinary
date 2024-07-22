4f9679e9d813578e413bc5861631721a320582ee
Sofia Ramirez Leiva <sofiaramirezleiva157@gmail.com>
2024-07-22 09:43:40 +0200

    created the annotations in each package

--- Patient/src/main/java/Veterinary/controller/AppointmentsController.java
+++ Patient/src/main/java/Veterinary/controller/AppointmentsController.java
@@ -1,4 +1,15 @@
 package Veterinary.controller;
 
+import Veterinary.service.AppointmentsService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/")
+@CrossOrigin(origins = "*")
+
 public class AppointmentsController {
+    @Autowired
+    AppointmentsService appointmentsService;
+
 }
--- Patient/src/main/java/Veterinary/service/AppointmentsService.java
+++ Patient/src/main/java/Veterinary/service/AppointmentsService.java
@@ -1,4 +1,13 @@
 package Veterinary.service;
 
+import Veterinary.Repository.AppointmentsRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+
 public class AppointmentsService {
+    @Autowired
+    AppointmentsRepository appointmentsRepository;
+
 }
