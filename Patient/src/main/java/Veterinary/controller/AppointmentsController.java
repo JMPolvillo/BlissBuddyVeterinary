99c1fa78fc4ec5732a97fb7825370a30a3e46d5a
Hilmar09 <hil.cris09@gmail.com>
2024-07-22 14:13:16 +0200

    merge feature

--- Patient/src/main/java/Veterinary/controller/AppointmentsController.java
+++ Patient/src/main/java/Veterinary/controller/AppointmentsController.java
@@ -3,6 +3,7 @@ package Veterinary.controller;
 import Veterinary.model.Appointments;
 import Veterinary.service.AppointmentsService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
