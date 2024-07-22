d301a82c0990924acd7af2f18d2ccdb7178ea05a
Sofia Ramirez Leiva <sofiaramirezleiva157@gmail.com>
2024-07-22 09:43:40 +0200

    created the annotations in each package

--- Patient/src/main/java/Veterinary/controller/PatientController.java
+++ Patient/src/main/java/Veterinary/controller/PatientController.java
@@ -1,4 +1,17 @@
 package Veterinary.controller;
 
+import Veterinary.service.PatientService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+@CrossOrigin(origins = "*")
+
 public class PatientController {
+    @Autowired
+    PatientService patientService;
+
 }
