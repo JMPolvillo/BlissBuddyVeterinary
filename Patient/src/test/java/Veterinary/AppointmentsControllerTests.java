2d137921be23ce824f5886f73c92b66934314930
Sofia Ramirez Leiva <sofiaramirezleiva157@gmail.com>
2024-07-26 09:11:58 +0200

    functional test

--- Patient/src/test/java/Veterinary/AppointmentsControllerTests.java
+++ Patient/src/test/java/Veterinary/AppointmentsControllerTests.java
@@ -2,25 +2,18 @@ package Veterinary;
 
 import Veterinary.controller.AppointmentsController;
 import Veterinary.model.Appointments;
-import Veterinary.model.Patient;
 import Veterinary.service.AppointmentsService;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.MockitoAnnotations;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
-import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
-import java.sql.Time;
 import java.time.LocalDate;
-import java.time.LocalDateTime;
 import java.time.LocalTime;
 
 import static org.mockito.ArgumentMatchers.any;
@@ -40,11 +33,14 @@ public class AppointmentsControllerTests {
 
     private ObjectMapper objectMapper = new ObjectMapper();
     private String appointmentJson;
+    private Appointments appointment;
 
     @BeforeEach
     void setUp() throws Exception {
+        // Registrar el módulo JavaTimeModule en el ObjectMapper
+        objectMapper.registerModule(new JavaTimeModule());
 
-        Appointments appointment = new Appointments();
+        appointment = new Appointments();
         appointment.setId(1);
         appointment.setTime(LocalTime.of(10, 5));
         appointment.setDate(LocalDate.of(2024, 7, 8));
@@ -52,8 +48,8 @@ public class AppointmentsControllerTests {
         appointment.setMotif("Pulgas");
         appointment.setState("Pasada");
 
+        // Convertir el objeto de cita a JSON
         appointmentJson = objectMapper.writeValueAsString(appointment);
-
     }
 
     @Test
@@ -67,3 +63,5 @@ public class AppointmentsControllerTests {
                 .andExpect(content().json(appointmentJson));
     }
 }
+
+
