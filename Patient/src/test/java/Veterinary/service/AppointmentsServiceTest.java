81086805d6be55b7aa8bb3b0a913deaef2130986
JMPolvillo <rothkildam@gmail.com>
2024-07-29 10:41:45 +0200

    added new tests for services

--- Patient/src/test/java/Veterinary/service/AppointmentsServiceTest.java
+++ Patient/src/test/java/Veterinary/service/AppointmentsServiceTest.java
@@ -1,4 +1,52 @@
+package Veterinary.service;
+
+import Veterinary.Repository.AppointmentsRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.verify;
+
 class AppointmentsServiceTest {
-  
+
+    @InjectMocks
+    private AppointmentsService appointmentsService;
+
+    @Mock
+    private AppointmentsRepository appointmentsRepository;
+
+    @BeforeEach
+    public void setUp(){
+        MockitoAnnotations.openMocks(this);
+    }
+
+    @Test
+    void createAppointment() {
+    }
+
+    @Test
+    void updateAppointments() {
+    }
+
+    @Test
+    void getAllAppointments() {
+    }
+
+    @Test
+    void getAppointmentById() {
+    }
+
+    @Test
+    void deleteAppointment() {
+    }
+
+    @Test
+    void deleteAppointmentByIdTest() {
+        int id = 1;
+        appointmentsService.deleteAppointmentById(id);
+        verify(appointmentsRepository).deleteById(id);
+    }
 }
--- Patient/src/test/java/Veterinary/service/PatientServiceTest.java
+++ Patient/src/test/java/Veterinary/service/PatientServiceTest.java
@@ -1,4 +1,54 @@
+package Veterinary.service;
+
+import Veterinary.Repository.PatientRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.verify;
+
 class PatientServiceTest {
-  
+    @InjectMocks
+    private PatientService patientService;
+
+    @Mock
+    private PatientRepository patientRepository;
+
+    @BeforeEach
+    public void setUp(){
+        MockitoAnnotations.openMocks(this);
+    }
+
+    @Test
+    void createPatient() {
+    }
+
+    @Test
+    void getAllPatients() {
+    }
+
+    @Test
+    void getPatientById() {
+    }
+
+    @Test
+    void updatePatient() {
+    }
+
+    @Test
+    void deletePatient() {
+    }
+
+    @Test
+    void deletePatientByIdTest() {
+    int id = 1;
+    patientService.deletePatientById(id);
+    verify(patientRepository).deleteById(id);
+    }
+
+
 }
