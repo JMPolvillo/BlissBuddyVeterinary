5b6c45145ac3c0f39c751e98376dfadeb4ac28a4
Sofia Ramirez Leiva <sofiaramirezleiva157@gmail.com>
2024-07-30 09:57:31 +0200

    added ObjectMapper

--- Patient/src/test/java/Veterinary/AppointmentsControllerTests.java
+++ Patient/src/test/java/Veterinary/AppointmentsControllerTests.java
@@ -86,6 +86,4 @@ public class AppointmentsControllerTests {
                 .andExpect(jsonPath("$.motif").value("Check"))
                 .andExpect(jsonPath("$.status").value("Earring"));
     }
-
-
 }
--- Patient/src/test/java/Veterinary/PatientControllerTests.java
+++ Patient/src/test/java/Veterinary/PatientControllerTests.java
@@ -1,6 +1,4 @@
 package Veterinary;
-
-import Veterinary.Repository.PatientRepository;
 import Veterinary.controller.PatientController;
 import Veterinary.model.Patient;
 import Veterinary.service.PatientService;
@@ -9,9 +7,6 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
 import org.springframework.test.web.servlet.MockMvc;
@@ -51,6 +46,8 @@ public class PatientControllerTests {
 	@Test
 	void createTestPatient() throws Exception {
 		when(patientService.createPatient(any(Patient.class))).thenReturn(patientBolita);
+
+		ObjectMapper objectMapper = new ObjectMapper();
 
 		mockMvc.perform(post("/patients")
 						.contentType(MediaType.APPLICATION_JSON)
